a368d5a87eab46a51897be29574966a0c22a38ef
MedAmineHm <mohamedamine.hammami@esprit.tn>
2024-02-19 10:26:55 +0100

    first commit

---
+++ src/views/examples/LoginPage.js
@@ -0,0 +1,238 @@
+import React from "react";
+import classnames from "classnames";
+import axios from "axios";
+import bcrypt from "bcryptjs";
+
+import { useNavigate } from "react-router-dom";
+
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardImg,
+  CardTitle,
+  Label,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+
+// core components
+import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
+import Footer from "components/Footer/Footer.js";
+
+export default function LoginPage() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [squares1to6, setSquares1to6] = React.useState("");
+  const [squares7and8, setSquares7and8] = React.useState("");
+  const [emailFocus, setEmailFocus] = React.useState(false);
+  const [passwordFocus, setPasswordFocus] = React.useState(false);
+  const navigate = useNavigate();
+  const handleLogin = async () => {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const userData = {
+      email,
+      password: hashedPassword,
+    };
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/auth/login",
+        userData
+      );
+
+      console.log("Login successful:", response.data);
+      navigate("/");
+      // Handle the successful login, such as storing the token in local storage, etc.
+    } catch (error) {
+      console.error("Error during login:", error);
+
+      if (error.response) {
+        // The request was made and the server responded with a non-success status
+        console.error("Response data:", error.response.data);
+        console.error("Response status:", error.response.status);
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.error("Request error:", error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.error("Error message:", error.message);
+      }
+
+      // Handle the login error, e.g., display an error message to the user
+    }
+    //
+  };
+
+  React.useEffect(() => {
+    document.body.classList.toggle("register-page");
+    document.documentElement.addEventListener("mousemove", followCursor);
+    // Specify how to clean up after this effect:
+    return function cleanup() {
+      document.body.classList.toggle("register-page");
+      document.documentElement.removeEventListener("mousemove", followCursor);
+    };
+  }, []);
+  const followCursor = (event) => {
+    let posX = event.clientX - window.innerWidth / 2;
+    let posY = event.clientY - window.innerWidth / 6;
+    setSquares1to6(
+      "perspective(500px) rotateY(" +
+        posX * 0.05 +
+        "deg) rotateX(" +
+        posY * -0.05 +
+        "deg)"
+    );
+    setSquares7and8(
+      "perspective(500px) rotateY(" +
+        posX * 0.02 +
+        "deg) rotateX(" +
+        posY * -0.02 +
+        "deg)"
+    );
+  };
+  return (
+    <>
+      <ExamplesNavbar />
+      <div className="wrapper">
+        <div className="page-header">
+          <div className="page-header-image" />
+          <div className="content">
+            <Container>
+              <Row>
+                <Col className="offset-lg-0 offset-md-3" lg="5" md="6">
+                  <div
+                    className="square square-7"
+                    id="square7"
+                    style={{ transform: squares7and8 }}
+                  />
+                  <div
+                    className="square square-8"
+                    id="square8"
+                    style={{ transform: squares7and8 }}
+                  />
+                  <Card className="card-register">
+                    <CardHeader>
+                      <CardImg
+                        alt="..."
+                        src={require("assets/img/square-purple-1.png")}
+                      />
+                      <CardTitle tag="h4">Login</CardTitle>
+                    </CardHeader>
+                    <CardBody>
+                      <Form className="form">
+                        <InputGroup
+                          className={classnames({
+                            "input-group-focus": emailFocus,
+                          })}
+                        >
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="tim-icons icon-email-85" />
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input
+                            placeholder="Email"
+                            type="text"
+                            onChange={(e) => setEmail(e.target.value)}
+                            onFocus={(e) => setEmailFocus(true)}
+                            onBlur={(e) => setEmailFocus(false)}
+                          />
+                        </InputGroup>
+                        <InputGroup
+                          className={classnames({
+                            "input-group-focus": passwordFocus,
+                          })}
+                        >
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="tim-icons icon-lock-circle" />
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input
+                            placeholder="Confirm Password"
+                            type="password" // Change this line
+                            onChange={(e) => setPassword(e.target.value)}
+                            onFocus={(e) => setPasswordFocus(true)}
+                            onBlur={(e) => setPasswordFocus(false)}
+                          />
+                        </InputGroup>
+
+                        <FormGroup check className="text-left">
+                          <Label check>
+                            <Input type="checkbox" />
+                            <span className="form-check-sign" />I agree to the{" "}
+                            <a
+                              href="#pablo"
+                              onClick={(e) => e.preventDefault()}
+                            >
+                              terms and conditions
+                            </a>
+                            .
+                          </Label>
+                        </FormGroup>
+                      </Form>
+                    </CardBody>
+                    <CardFooter>
+                      <Button
+                        className="btn-round"
+                        color="primary"
+                        size="lg"
+                        onClick={handleLogin}
+                      >
+                        Login
+                      </Button>
+                    </CardFooter>
+                  </Card>
+                </Col>
+              </Row>
+              <div className="register-bg" />
+              <div
+                className="square square-1"
+                id="square1"
+                style={{ transform: squares1to6 }}
+              />
+              <div
+                className="square square-2"
+                id="square2"
+                style={{ transform: squares1to6 }}
+              />
+              <div
+                className="square square-3"
+                id="square3"
+                style={{ transform: squares1to6 }}
+              />
+              <div
+                className="square square-4"
+                id="square4"
+                style={{ transform: squares1to6 }}
+              />
+              <div
+                className="square square-5"
+                id="square5"
+                style={{ transform: squares1to6 }}
+              />
+              <div
+                className="square square-6"
+                id="square6"
+                style={{ transform: squares1to6 }}
+              />
+            </Container>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
+}
---
+++ src/views/examples/ProfilePage.js
@@ -0,0 +1,119 @@
+import React from "react";
+// javascript plugin used to create scrollbars on windows
+import PerfectScrollbar from "perfect-scrollbar";
+// reactstrap components
+import {
+  Card,
+  CardBody,
+  Table,
+  TabContent,
+  TabPane,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+
+// core components
+import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
+import Footer from "components/Footer/Footer.js";
+
+const carouselItems = [
+  {
+    src: require("assets/img/denys.jpg"),
+    altText: "Slide 1",
+    caption: "Big City Life, United States",
+  },
+  {
+    src: require("assets/img/fabien-bazanegue.jpg"),
+    altText: "Slide 2",
+    caption: "Somewhere Beyond, United States",
+  },
+  {
+    src: require("assets/img/mark-finn.jpg"),
+    altText: "Slide 3",
+    caption: "Stocks, United States",
+  },
+];
+
+let ps = null;
+
+export default function ProfilePage() {
+  const [tabs] = React.useState(1);
+  React.useEffect(() => {
+    if (navigator.platform.indexOf("Win") > -1) {
+      document.documentElement.className += " perfect-scrollbar-on";
+      document.documentElement.classList.remove("perfect-scrollbar-off");
+      let tables = document.querySelectorAll(".table-responsive");
+      for (let i = 0; i < tables.length; i++) {
+        ps = new PerfectScrollbar(tables[i]);
+      }
+    }
+    document.body.classList.toggle("profile-page");
+    // Specify how to clean up after this effect:
+    return function cleanup() {
+      if (navigator.platform.indexOf("Win") > -1) {
+        ps.destroy();
+        document.documentElement.className += " perfect-scrollbar-off";
+        document.documentElement.classList.remove("perfect-scrollbar-on");
+      }
+      document.body.classList.toggle("profile-page");
+    };
+  }, []);
+  return (
+    <>
+      <ExamplesNavbar />
+      <div className="wrapper">
+        <div className="page-header">
+          <img
+            alt="..."
+            className="dots"
+            src={require("assets/img/dots.png")}
+          />
+          <img
+            alt="..."
+            className="path"
+            src={require("assets/img/path4.png")}
+          />
+          <Container className="align-items-center">
+            <Row>
+              <Col className="ml-auto mr-auto" lg="4" md="6">
+                <Card className="card-coin card-plain">
+                  <CardBody>
+                    <TabContent
+                      className="tab-subcategories"
+                      activeTab={"tab" + tabs}
+                    >
+                      <TabPane tabId="tab3">
+                        <Table className="tablesorter" responsive>
+                          <thead className="text-primary">
+                            <tr>
+                              <th className="header">Latest Crypto News</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <td>The Daily: Nexo to Pay on Stable...</td>
+                            </tr>
+                            <tr>
+                              <td>Venezuela Begins Public of Nation...</td>
+                            </tr>
+                            <tr>
+                              <td>PR: BitCanna – Dutch Blockchain...</td>
+                            </tr>
+                          </tbody>
+                        </Table>
+                      </TabPane>
+                    </TabContent>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <div className="section"></div>
+        <section className="section"></section>
+        <Footer />
+      </div>
+    </>
+  );
+}
